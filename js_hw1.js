17a991df201d8b562b420da34d62c7241c1c3604
Anton-phg <warriorato@gmail.com>
2025-04-12 14:38:45 +0000

    Homework

---
+++ js_hw1.js
@@ -0,0 +1,86 @@
+// TASK 2 +
+
+// alert ('Anton');
+
+
+// TASK 3 +
+
+// let oldNumber;
+// let newNumber;
+// oldNumber = 5;
+// newNumber = 11;
+// alert (oldNumber);
+// alert (newNumber);
+// oldNumber = newNumber;
+// alert (oldNumber);
+// alert (newNumber);
+
+
+// TASK 4 +
+
+// const user = {
+//     name: "Antony", //String
+//     age: 34, // Number
+//     married: false, // Boolean
+//     online: undefined,  //undefined
+//     debt: null, //null
+// };
+
+
+// TASK 5 +
+
+// let isAdult = confirm("Do you adult?");
+
+// alert( isAdult );
+
+// TASK 6+
+
+// let yourName;
+// let yourSurname;
+// let yourGroup;
+// let yearOfBirth;
+// let statusMarried;
+// let doYouOnlain;
+// let yourDebt;
+// yourName = "Antony";
+// yourSurname = "Pysmenskyi";
+// yourGroup = "AFN Full Stack";
+// yearOfBirth = 1991;
+// statusMarried = false;
+// doYouOnlain = undefined;
+// yourDebt = null;
+// alert  (`yearOfBirth = ${yearOfBirth} : ${typeof  yearOfBirth}`);
+// alert  (`statusMarried = ${statusMarried} : ${typeof statusMarried}`);
+// alert  (`yourName = ${yourName} : ${typeof yourName}`);
+// alert  (`yourSurname = ${yourSurname} : ${typeof  yourSurname}`);
+// alert  (` yourGroup : ${yourGroup} : ${typeof yourGroup}`);
+// alert  (`doYouOnlain = ${doYouOnlain} : ${typeof doYouOnlain}`);
+// alert  (`yourDebt = ${yourDebt} : ${typeof yourDebt}`); //Тут видає результат 'object'
+
+// TASK 7 +
+
+// let userName = prompt ('What is your name?', 'Vasya');
+// let userEmail = prompt ('What is your email?', 'vasya@mail.com');
+// let userPassword = prompt ('What is your password?', 'Vasya0000@');
+// alert (`Dear ${userName}, your email is ${userEmail}, your password is ${userPassword}`)
+
+
+// TASK 8+
+
+// let second = 1;
+// let minute = 60;
+// let hour = 60;
+// let day = 24;
+// let mounth = prompt ('Введіть кількість днів в місяці від 28 до 31', 31)
+// let secondsInMinute;
+// let secondsInHour;
+// let secondsInDay;
+// let secondsInMounth;
+// secondsInMinute = second * minute;
+// alert (`В хвилині ${secondsInMinute} секунд`)
+// secondsInHour = second * minute * hour;
+// alert (`В годині ${secondsInHour} секунд`)
+// secondsInDay = second * minute * hour * day;
+// alert (`В добі ${secondsInDay} секунд`)
+// secondsInMounth = second * minute * hour * day * mounth;
+// alert (`В місяці з ${mounth} днів ${secondsInMounth} секунд`)
